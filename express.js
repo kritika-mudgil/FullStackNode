a4029e83243dd98a5a8356898200ec6e9ba5a5aa
kritika-mudgil <kritikamudgil10@gmail.com>
2025-09-15 23:19:59 +0530

    Add files via upload

--- express.js
+++ express.js
@@ -1,43 +1,28 @@
 const express=require("express")
 const app=express()
-const data=require("./search")
+const data=require("./data")
 
 app.use(express.json())
 
 
-app.get("/users/:id",(req,res)=>{
-    const id=parseInt (req.params.id)
-    const user=data.users.find(u=>u.id===id)
-    if(user){
-        res.json({
-            message:"successfully found",
-            data:user
-        })
-    }
-    else{
-        res.json({
-            message:"not found",
-            status:404
-        })
-    }
+app.get("/users",(req,res)=>{
+// res.send(data.users)
+res.json({
+    message:"User added successfully",
+    data:data.users,
+    time:new Date().toLocaleString()
+    })
 })
 
-app.delete("/users/:id",(req,res)=>{
-    const id=parseInt(req.params.id)
-    const index=data.users.findIndex(u=>u.id===id)
-    if(index==-1){
-        const deleteduser=data.users.splice(index,1)
-        res.json({
-            message:"deleted successfully",
-            data:deleteduser[0]
-        })
-    }
-    else{
-        res.json({
-            message:"not deleted"
-        })
-    }
-})
+// app.post("/users",(req,res)=>{
+//     const new_data=req.body
+//     data.users.push(new_data)
+//     res.json({
+//         message:"data successfully loaded",
+//         data:data.users,
+//         time:new Date().toLocaleString()
+//     })
+// })
 app.listen(7000,()=>{
     console.log("server started")
 })
